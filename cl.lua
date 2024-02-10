c8b43423ae1216c6a6a9e9d8f2ac03678525d2f1
DX HIGH <107764618+DX1307@users.noreply.github.com>
2024-02-10 23:44:06 +0700

    Update cl.lua

--- cl.lua
+++ cl.lua
@@ -5,26 +5,17 @@ local count = 0
 CreateThread(function()
 	while true do
 		local sleep = true
-		local GetPlayerPed = GetPlayerPed(-1)
+		local playerPed = PlayerPedId()
 		Wait(9)
-		local gotWeapon, hasWeapon = GetCurrentPedWeapon(GetPlayerPed, true)
+		local gotWeapon, hasWeapon = GetCurrentPedWeapon(playerPed, true)
+		
 		if gotWeapon ~= false then
-			
 			for k, v in pairs(Config.Key) do
 				if IsControlPressed(0, v) then
 					sleep = false
 					Anti = true
-					CreateThread(function()
-						while Anti do
-							local sleep = true
-							local PlayerPedId = PlayerPedId()
-							DisablePlayerFiring(PlayerPedId,true)
-							DisableControlAction(0, 140, true)
-							DisableControlAction(0, 25, true)
-							Wait(9)
-						end
-					end)
-					stopWeapon(hasWeapon)
+					DisableActionsWhileAnti(playerPed)
+					stopWeapon(playerPed, hasWeapon)
 				end
 			end
 
@@ -33,44 +24,46 @@ CreateThread(function()
 				count = count + 0.1
 				if count > Config.HoldSpacebar then
 					AntiSpacebar = true
-					CreateThread(function()
-						while AntiSpacebar do
-							local PlayerPedId = PlayerPedId()
-							DisablePlayerFiring(PlayerPedId,true)
-							DisableControlAction(0, 140, true)
-							DisableControlAction(0, 25, true)
-							DisableControlAction(0, Config.KeySpacebar, true)
-							if (IsDisabledControlJustPressed(0, Config.KeySpacebar)) then
-								AntiSpam('AntiSpacebar', 'ห้ามกด Spacebar ค้าง')
-							end
-							Wait(9)
-						end
-					end)
-					AntiSpacebarfun(hasWeapon)
+					DisableActionsWhileAnti(playerPed)
+					AntiSpacebarfun(playerPed, hasWeapon)
 				end
 			end
 		end
+
 		if sleep then
 			Wait(200)
 		end
 	end
 end)
 
-local UnarmedHash = `WEAPON_UNARMED`
-
-function stopWeapon(hasWeapon)
-	local PlayerPedId = PlayerPedId()
-	SetCurrentPedWeapon(PlayerPedId, UnarmedHash, true)
-	RemoveWeaponFromPed(PlayerPedId, hasWeapon)
+function stopWeapon(playerPed, hasWeapon)
+	SetCurrentPedWeapon(playerPed, `WEAPON_UNARMED`, true)
+	RemoveWeaponFromPed(playerPed, hasWeapon)
 	Wait(Config.DelayAntiKey * 1000)
 	Anti = false
 end
 
-function AntiSpacebarfun(hasWeapon)
-	local PlayerPedId = PlayerPedId()
-	SetCurrentPedWeapon(PlayerPedId, UnarmedHash, true)
-	RemoveWeaponFromPed(PlayerPedId, hasWeapon)
+function AntiSpacebarfun(playerPed, hasWeapon)
+	SetCurrentPedWeapon(playerPed, `WEAPON_UNARMED`, true)
+	RemoveWeaponFromPed(playerPed, hasWeapon)
 	Wait(Config.DelayAntiSpacebar * 1000)
 	AntiSpacebar = false
 	count = 0
+end
+
+function DisableActionsWhileAnti(playerPed)
+	CreateThread(function()
+		while Anti or AntiSpacebar do
+			DisablePlayerFiring(playerPed, true)
+			DisableControlAction(0, 140, true)
+			DisableControlAction(0, 25, true)
+			if AntiSpacebar then
+				DisableControlAction(0, Config.KeySpacebar, true)
+				if IsDisabledControlJustPressed(0, Config.KeySpacebar) then
+					AntiSpam('AntiSpacebar', 'ห้ามกด Spacebar ค้าง')
+				end
+			end
+			Wait(9)
+		end
+	end)
 end
